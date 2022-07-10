348f749f53a58d14d5c0728c3a8c0edbb77b8a66
JevyZhu <jingweizhucn@126.com>
2022-07-10 16:37:07 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const core = require('@actions/core');
 const spawnSync = require('child_process').spawnSync
 
+function chooseBinary() {
+    if (process.platform === 'linux' && process.arch === 'x64') {
+        return "main-linux-amd64"
+    }
+}
+
 try {
   const file = core.getInput('file');
   console.log(`File pattern: ${file}!`);
   const maxcur = core.getInput('max_cur');
   console.log(`Max tasks numer : ${maxcur}!`);
-  const returns  = spawnSync(`${__dirname}/s3uploader -p "${file}" -m ${maxcur}`, {
+  const binary = chooseBinary()
+  const returns  = spawnSync(`${__dirname}/${binary} -p "${file}" -m ${maxcur}`, {
     stdio: 'inherit',
     shell: true
   });
