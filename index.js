06c9bb737c57bffa277a655eeb09c765da8030e7
JevyZhu <jingweizhucn@126.com>
2022-07-10 13:03:04 +0800

    update

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 const core = require('@actions/core');
-const spawn = require('child_process').spawn
+const spawnSync = require('child_process').spawnSync
 
 try {
   const file = core.getInput('file');
   console.log(`File pattern: ${file}!`);
   const maxcur = core.getInput('max_cur');
   console.log(`Max tasks numer : ${maxcur}!`);
-  const child = spawn(`./s3uploader -p "${file}" -m ${maxcur}`, {
+  const returns  = spawnSync(`${__dirname}/s3uploader -p "${file}" -m ${maxcur}`, {
     stdio: 'inherit',
     shell: true
   });
-  // child.on('exit', function (code, signal) {
-  //   console.log('child process exited with ' +
-  //     `code ${code} and signal ${signal}`);
-  // });
-  // child.stdout.on('data', (data) => {
-  //   console.log(`${data}`);
-  // });
-  // child.stderr.on('data', (data) => {
-  //   console.log(`${data}`);
-  // });
 } catch (error) {
   core.setFailed(error.message);
 }
